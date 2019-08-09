aa46411e973e68b13e5c13d9c477d963beeaae73
Annu <43414928+annu12340@users.noreply.github.com>
2019-08-09 18:02:11 +0530

    Create what_the_text.py

---
+++ what_the_text.py
@@ -0,0 +1,31 @@
+import cv2
+import pytesseract
+from PIL import Image
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    cv2.imshow("test", frame)
+    if not ret:
+        break
+    k = cv2.waitKey(1)
+
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        print(pytesseract.image_to_string(img_name))
+        img_counter += 1
+
+cam.release()
+cv2.destroyAllWindows()
+
